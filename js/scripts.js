050866636396daedbb4200e36a25f724334f94b0
Alvin Ng and Tony Fabery <alvin@>
2016-06-13 16:00:35 -0700

    add editing of checklist item and removing and moving the value to completed tasks list

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,41 @@ $(function() {
     for (var i = 0; i < doList.length; i++) {
       // console.log(doList[i]);
       newDo = new ToDo(doList[i]);
-      $('ul#output').append('<li class="do">' + newDo.do + "</li>");
+      $('ul#output').append('<li class="do">' +
+                              '<span class="liItem">' + newDo.do + '</span>' +
+                              '<span class="edit">' +
+                                '<input type="text" class="newVal">' +
+                                '<input type="submit" value="Edit" class="submit">' +
+                              '</span>' +
+                              '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' +
+                              '<button class="btn remove" type="remove">' +
+                                '<span class="glyphicon glyphicon-remove"></span>' +
+                              '</button>' +
+                              '&nbsp&nbsp' +
+                              '<button class="btn edit-button" type="edit">' +
+                                '<span class="glyphicon glyphicon-edit"></span>' +
+                              '</button>' +
+                            '</li>');
       // console.log(newDo);
     };
 
     // console.log(newDo);
-    $('.do').click(function() {
+    $('.remove').click(function() {
       // console.log('remove');
-      this.remove('.do');
+      $(this).parent().remove();
+      $('ul#complete').append('<li class="complete">' + $(this).parent().text() + "</li>");
     });
+
+    $('.edit-button').click(function() {
+      $(this).siblings('.liItem').hide().siblings('.edit').show();
+      $(this).siblings('.edit').children('.submit').click(function() {
+        var editVal = $(this).siblings('.newVal').val();
+        // console.log(editVal);
+        $(this).parent().siblings('.liItem').text(editVal);
+        $(this).parent().siblings('.liItem').show().siblings('.edit').hide();
+      });
+    });
+
+    $('input#do').val('')
   });
 });
